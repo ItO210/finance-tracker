065ccc78bffcccf04b605b85360f2744f830239f
Carlos Ito <carlos.ito.0210@gmail.com>
2025-01-02 23:28:06 -0600

    added recurring payment routes

---
+++ Backend/routes/recurringPaymentRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const recurringPaymentController = require("../controllers/recurringPaymentController");
+
+router.post(
+  "/recurringPayment",
+  recurringPaymentController.createRecurringPayment
+);
+router.get(
+  "/recurringPayment/:id",
+  recurringPaymentController.getRecurringPaymentById
+);
+router.get(
+  "/recurringPayments",
+  recurringPaymentController.getAllRecurringPayments
+);
+router.put(
+  "/recurringPayment/:id",
+  recurringPaymentController.updateRecurringPaymentById
+);
+router.delete(
+  "/recurringPayment/:id",
+  recurringPaymentController.deleteRecurringPaymentById
+);
+router.get(
+  "/upcomingRecurringPayments",
+  recurringPaymentController.getUpcomingRecurringPayments
+);
